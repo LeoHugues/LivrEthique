ddc77fa74c2c0d0559d415f18ec106ee2169657e
Pierrot34990 <p.baumes@gmail.com>
2017-11-28 16:16:32 +0100

    WIP

--- src/AppBundle/Controller/ContactController.php
+++ src/AppBundle/Controller/ContactController.php
@@ -9,9 +9,7 @@ class ContactController extends Controller
 {
     public function contactAction(Request $request, \Swift_Mailer $mailer)
     {
-
         if($request->isMethod('POST')) {
-
             $lastname = $request->request->get('lastname');
             $firstname = $request->request->get('firstname');
             $object = $request->request->get('object');
@@ -34,6 +32,11 @@ class ContactController extends Controller
                 );
 
             $mailer->send($message);
+
+            $request->getSession()
+                ->getFlashBag()
+                ->add('success', 'Votre message a bien été envoyé!')
+            ;
         }
 
         return $this->render('contact/contact.html.twig', [
