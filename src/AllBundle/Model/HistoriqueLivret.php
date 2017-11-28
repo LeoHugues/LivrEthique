90d45666da8b374fd8f4a77a0bd06fc552b70fb0
root <root@momo.fr>
2017-11-28 14:03:34 +0000

    classes

---
+++ src/AllBundle/Model/HistoriqueLivret.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Nil
+ * Date: 28/11/2017
+ * Time: 14:31
+ */
+class HistoriqueLivret
+{
+    public $idTransaction;
+    public $solde;
+    public $credit;
+    public $debiteur;
+    public $crediteur;
+    public $label;
+    public $reference;
+
+    function __construct()
+    {
+
+    }
+}
---
+++ src/AllBundle/Model/Livret.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Nil
+ * Date: 28/11/2017
+ * Time: 14:31
+ */
+class Livret
+{
+    public $id;
+    public $label;
+    public $solde;
+    public $id_historiqueLivret;
+    public $dateOuverture;
+    public $tauxInteret;
+    public $isEthic;
+    public $IBAN;
+    public $BIC;
+
+    function __construct()
+    {
+
+    }
+}
