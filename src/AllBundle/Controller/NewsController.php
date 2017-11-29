d771beb877e2efdcc252aaf8d2297143300b6422
root <root@momo.fr>
2017-11-29 13:37:03 +0000

    news

--- src/AllBundle/Controller/NewsController.php
+++ src/AllBundle/Controller/NewsController.php
@@ -26,14 +26,14 @@ class NewsController extends Controller
         $news->setTitle("Nouvelle prime pour les entreprises green IT");
         $news->setAddedDate("29/11/2017");
         $news->setNewsCategoryId("divers");
-        $news->setContent("En effet, les entreprises vont recevoir à partir du 01/12/2017 une nouvelle prime pour financer les projets ayant pour principe de violer les enfants. Deux fois.");
+        $news->setContent("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
         array_push($newsTab, $news);
 
         $news = new News();
         $news->setTitle("2018 en ligne de mire pour Greenpeace");
         $news->setAddedDate("02/11/2017");
         $news->setNewsCategoryId("écologie");
-        $news->setContent("Greenpeace annonce un partenariat avec ma bite. Oui oui, ma bite.");
+        $news->setContent("Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?");
         array_push($newsTab, $news);
 
         return $this->render('All/news/news.html.twig', [
